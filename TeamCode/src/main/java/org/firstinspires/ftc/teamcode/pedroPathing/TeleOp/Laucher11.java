0ce461326c241ce4553923922c2ababe03ab0df0
GnHk28 <Gustavo.nhavida@gmail.com>
2026-04-28 09:42:26 -0300

    Novas alterações no código LauncherTest e Omni_Move. + Novo Classe "Laucher11"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOp/Laucher11.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOp/Laucher11.java
@@ -38,14 +38,15 @@ import org.firstinspires.ftc.teamcode.pedroPathing.TeleOp.Omni_Move;
 import java.util.function.Supplier;
 
 
-@TeleOp(name = "First Launcher / TestMode", group = "TeleOp")
-public class LauncherTest extends LinearOpMode{
+@TeleOp(name = "Second Launcher / BasedMode", group = "TeleOp")
+public class Laucher11 extends LinearOpMode{
     private Omni_Move Drive;
     private Servo s1;
-    private DcMotor In1;
-    private DcMotor In2;
-    private DcMotor sh1;
-    private DcMotor sh2;
+
+    //private DcMotor In1;
+    //private DcMotor In2;
+    //private DcMotor sh1;
+    //private DcMotor sh2;
     private Follower follower;
     public static Pose startingPose;
     private boolean automatedDrive;
@@ -73,25 +74,37 @@ public class LauncherTest extends LinearOpMode{
 
         Drive = new Omni_Move();
 
-        In1 = hardwareMap.get(DcMotor.class,"c1");
-        sh1 = hardwareMap.get(DcMotor.class,"c2");
-        sh2 = hardwareMap.get(DcMotor.class, "c3");
-        ex3 = hardwareMap.get(DcMotor.class, "c4");
-        s1 = hardwareMap.get(Servo.class, "s1");
+        DcMotorEx c1z = hardwareMap.get(DcMotorEx.class, "c1");
+        DcMotorEx c2z = hardwareMap.get(DcMotorEx.class, "c2");
+        DcMotorEx c3z = hardwareMap.get(DcMotorEx.class,"c3");
+        DcMotorEx c4z = hardwareMap.get(DcMotorEx.class,"c4");
 
-        In1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        In2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        sh1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Servo s1z = hardwareMap.get(Servo.class,"s1");
+        Servo s2z = hardwareMap.get(Servo.class,"s2");
 
-        In1.setDirection(DcMotor.Direction.REVERSE);
-        In2.setDirection(DcMotor.Direction.REVERSE);
-        sh1.setDirection(DcMotor.Direction.REVERSE);
+        c1z.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        c2z.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        c3z.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        c1z.setDirection(DcMotor.Direction.REVERSE);
+        c2z.setDirection(DcMotor.Direction.REVERSE);
+        c3z.setDirection(DcMotor.Direction.REVERSE);
 
         boolean InT1 = false;
-        boolean InT2 = false;
+
+
+        boolean shotZ = false;
         boolean BInT1 = false;
+        boolean shotB = false;
+
+        boolean shot1 = false;
+        boolean shot2 = false;
+
         boolean shI = false;
         boolean shI2 = false;
+
+        double sp = 0.7; // sp = shotPower
+
         boolean Servo = false;
 
         boolean coreL = false;
@@ -132,68 +145,77 @@ public class LauncherTest extends LinearOpMode{
                 );
             }
 
-            //Automated PathFollowing
-            /*if (gamepad1.aWasPressed()) {
-                follower.followPath(pathChain.get());
-                automatedDrive = true;
+            telemetry.addData("Intake1: ", ativar);
+            telemetry.addData("Intake1: ", ativar);
+
+            InT1 = gamepad1.right_trigger > 0.3;
+
+            if(InT1) {
+                c1z.setPower(1);
+                ativar = "Ligado";
+            } else {
+                c1z.setPower(0);
+                ativar = "Desligado";
             }
 
-            //Stop automated following if the follower is done
-            if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
-                follower.startTeleopDrive();
-                automatedDrive = false;
+            BInT1 = gamepad1.left_trigger > 0.3;
+
+            if(BInT1) {
+                c1z.setPower(1);
+                ativar = "Ligado";
+            } else {
+                c1z.setPower(0);
+                ativar = "Desligado";
             }
 
-            //Slow Mode
-            if (gamepad1.rightBumperWasPressed()) {
-                slowMode = !slowMode;
+            if(gamepad1.y && !shotZ) {
+                c2z.setPower(sp);
+                c3z.setPower(sp);
+                shotZ = true;
+                sleep(100);
+            } else if(gamepad1.y && shotZ) {
+                c2z.setPower(0);
+                c3z.setPower(0);
+                shotZ = false;
+                sleep(100);
             }
 
-            //Optional way to change slow mode strength
-            if (gamepad1.xWasPressed()) {
-                slowModeMultiplier += 0.25;
+            if(gamepad1.x && !shotB) {
+                c2z.setPower(-sp);
+                c3z.setPower(-sp);
+                shotB = true;
+                sleep(100);
+            } else if(gamepad1.x && shotB) {
+                c2z.setPower(0);
+                c3z.setPower(0);
+                shotB = false;
+                sleep(100);
             }
 
-            //Optional way to change slow mode strength
-            if (gamepad2.yWasPressed()) {
-                slowModeMultiplier -= 0.25;
+            if(gamepad1.a && !shot1) {
+                c2z.setPower(-1);
+                shot1 = true;
+                sleep(100);
+            } else if(gamepad1.a && shot1) {
+                c2z.setPower(0);
+                shot1 = false;
+                sleep(100);
             }
 
-            telemetryM.debug("position", follower.getPose());
-            telemetryM.debug("velocity", follower.getVelocity());
-            telemetryM.debug("automatedDrive", automatedDrive);
+            if(gamepad1.b && !shot2) {
+                c3z.setPower(-1);
+                shot2 = true;
+                sleep(100);
+            } else if(gamepad1.b && shot2) {
+                c3z.setPower(0);
+                shot2 = false;
+                sleep(100);
+            }
+
+
+            telemetry.update();
         }
-        */
-            /*double y = -gamepad1.left_stick_y; // frente/trás
-            double x = gamepad1.left_stick_x;  // lateral
-            double rot = gamepad1.right_stick_x; // rotação */
-
-        telemetry.addData("Intake1: ", ativar);
-
-        InT1 = gamepad1.right_trigger > 0.3;
-
-        if(InT1) {
-            In1.setPower(1);
-            ativar = "Ligado";
-        } else {
-            In1.setPower(0);
-            ativar = "Desligado";
-        }
-
-        BInT1 = gamepad1.left_trigger > 0.3;
-
-        if(BInT1) {
-            In1.setPower(-1);
-            ativar = "Ligado";
-        } else {
-            In1.setPower(0);
-            ativar = "Desligado";
-        }
-
-
-        telemetry.update();
     }
-}
 
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOp/Omni_Move.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOp/Omni_Move.java
@@ -1,26 +1,25 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.TeleOp;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp (name = "Move", group = "TeleOp")
+@TeleOp (name = "Move / Omni_Mover", group = "TeleOp")
 public class Omni_Move extends LinearOpMode {
 
     boolean press = false;
     boolean pressr = false;
     boolean pressr1 = false;
     boolean pressr2 = false;
+    boolean tdc = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         DcMotorEx c1 = hardwareMap.get(DcMotorEx.class, "c1");
         DcMotorEx c2 = hardwareMap.get(DcMotorEx.class, "c2");
+        DcMotorEx c3 = hardwareMap.get(DcMotorEx.class,"c3");
+        DcMotorEx c4 = hardwareMap.get(DcMotorEx.class,"c4");
 
         double currentPowerBase = 1;
 
@@ -50,22 +49,36 @@ public class Omni_Move extends LinearOpMode {
                 sleep(100);
             }
 
+            if(gamepad1.a && !tdc){
+                c3.setPower(1);
+                tdc = true;
+                sleep(100);
+            } else if(gamepad1.a && tdc) {
+                c3.setPower(0);
+                tdc = false;
+                sleep(100);
+            }
+
             if(gamepad1.y && !pressr1) {
                 c2.setPower(1);
+                c3.setPower(1);
                 pressr1 = true;
                 sleep(100);
             } else if(gamepad1.y && pressr1) {
                 c2.setPower(0);
+                c3.setPower(0);
                 pressr1 = false;
                 sleep(100);
             }
 
             if(gamepad1.x && !pressr2) {
                 c2.setPower(-1);
+                c3.setPower(-1);
                 pressr2 = true;
                 sleep(100);
             } else if(gamepad1.x && pressr2) {
                 c2.setPower(0);
+                c3.setPower(0);
                 pressr2 = false;
                 sleep(100);
             }
