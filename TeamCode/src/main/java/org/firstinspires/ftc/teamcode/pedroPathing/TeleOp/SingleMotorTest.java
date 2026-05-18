a1bdf1574fdd23afb7284657bec7249c99ec7557
Avechester <conanavechester@gmail.com>
2026-05-18 19:32:29 -0300

    Melhorando SingleMotorTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOp/SingleMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TeleOp/SingleMotorTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.pedroPathing.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp (name = "Teste Single Motor", group = "TeleOp")
 public class SingleMotorTest extends LinearOpMode {
@@ -12,7 +13,7 @@ public class SingleMotorTest extends LinearOpMode {
 
         DcMotorEx tower = hardwareMap.get(DcMotorEx.class, "tower");
 
-        double power = 0.5;
+        double power = 1;
 
         boolean right = false;
         boolean left = false;
@@ -22,11 +23,13 @@ public class SingleMotorTest extends LinearOpMode {
         while (opModeIsActive()) {
 
             if(gamepad1.right_bumper && !right) {
-                power =+ 1;
+                power =+ 0.1;
+                power = Range.clip(power, 0.0, 1.0);
             }
             right = gamepad1.right_bumper;
             if(gamepad1.left_bumper && !left) {
-                power =- 1;
+                power =- 0.1;
+                power = Range.clip(power, 0.0, 1.0);
             }
             left = gamepad1.left_bumper;
 
@@ -37,6 +40,11 @@ public class SingleMotorTest extends LinearOpMode {
             } else {
                 tower.setPower(0);
             }
+
+            telemetry.addLine("Pressione b ou x");
+            telemetry.addData("Power:", power);
+
+            telemetry.update();
         }
     }
 
